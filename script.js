7ca1f7989d35c8120d08e59c0b7a4f7ee032e4ba
rywilldev <rywilldav@gmail.com>
2023-04-28 08:38:33 -0400

    add score buttons

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ let homeScore = 0;
 let guestScore = 0;
 document.querySelector("#home-score-el").textContent = homeScore;
 document.querySelector("#guest-score-el").textContent = guestScore;
+
+function addHomeOne() {
+    homeScore.textContent = homeScore += 1;
+    document.querySelector("#home-score-el").textContent = homeScore;
+    console.log(homeScore);
+}
+
+function addHomeTwo() {
+    homeScore.textContent = homeScore += 2;
+    document.querySelector("#home-score-el").textContent = homeScore;
+    console.log(homeScore);
+}
+
+function addHomeThree() {
+    homeScore.textContent = homeScore += 3;
+}
+
+function addGuestOne() {
+    guestScore.textContent = guestScore += 1;
+}
+
+function addHomeTwo() {
+    guestScore.textContent = guestScore += 2;
+}
+
+function addHomeThree() {
+    guestScore.textContent = guestScore += 3;
+}
